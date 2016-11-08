3a2935a8e4f63c42597e8facd26cdd3da2dd9ef5
Andrew Wang <cngwang6@gmail.com>
2016-11-07 23:47:32 -0500

    Updated router with *actual* router functions/calls.

--- app/app.js
+++ app/app.js
@@ -1,30 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import Home from './components/Home';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 
 class HomePage extends React.Component {
   render() {
-    return (
-      <Home />
-    );
-  }
-}
-=======
-import NavBar from './components/navbar';
->>>>>>> c61af11e5eb0ac8cfa5ee672fbc66451987c74a5
-
-class Navbar extends React.Component {
-  render(){
-    return (
-      <NavBar />
-    );
+    return <Home />;
   }
 }
 
-<<<<<<< HEAD
-ReactDOM.render(<HomePage />
-),document.getElementById('root');
-=======
-ReactDOM.render(document.getElementById('bikePage'));
->>>>>>> c61af11e5eb0ac8cfa5ee672fbc66451987c74a5
+class NavBar extends React.Component {
+  render() {
+    return <NavBar />;
+  }
+}
+class MyBuild extends React.Component {
+  render() {
+    return <MyBuild />;
+  }
+}
+class SavedBuilds extends React.Component {
+  render() {
+    return (
+      <SavedBuilds />
+      );
+  }
+}
+class Account extends React.Component {
+  render() {
+    return (
+      <Account />
+      );
+  }
+}
+class App extends React.Component {
+  render() {
+    return (<div>
+              { this.props.children }
+            </div>)
+  }
+}
+
+ReactDOM.render((
+  <Router history={ browserHistory }>
+    <Route
+           path="/"
+           component={ App }>
+      { /* Show the Feed at / */ }
+      <IndexRoute component={ HomePage } />
+      <Route
+             path="account/:id"
+             component={ Account } />
+             <Route
+             path="MyBuild/:id"
+             component={ MyBuild } />
+                   <Route
+             path="SavedBuilds/:id"
+             component={ SavedBuilds } />
+    </Route>
+  </Router>
+  ),
+  document.getElementById('bikePage'));
