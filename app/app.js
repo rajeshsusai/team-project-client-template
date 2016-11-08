49361ad639c6774a58dce4780c793fb335045271
Peter Nguyen <Peternguyen0411@gmail.com>
2016-11-08 17:59:32 -0500

    Status

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home';
+
 import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 
 class HomePage extends React.Component {
