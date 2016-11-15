534b40efe330b1e177ef2fc0e31b6a4d6d40b488
Peter Nguyen <Peternguyen0411@gmail.com>
2016-11-14 20:59:30 -0500

    updating account

--- app/components/Account.js
+++ app/components/Account.js
@@ -31,40 +31,39 @@ export default class Account extends React.Component {
     var username = this.state.user_name;
 
     return (
-      <div className="container jumbotron accountcontrol">
-        <div action="My-Account-Page form-horizontal" method="post">
-          <div className="form-group">
-            <label className="control-label col-sm-2 left" htmlFor="first name" >First Name</label>
-            <div className="col-sm-10">
-              <input type="text" className="form-control" id="first name" placeholder = {firstName}/>
-              </div>
-            </div>
+
+      <div className="body-container">
+        <div className="col-md-2">
+        </div>
+        <div className="col-md-8 text-align-left">
+          <div className="panel panel-default">
             <div className="form-group">
-              <label className="control-label col-sm-2 left" htmlFor="last name" >Last Name</label>
-              <div className="col-sm-10">
-                <input type="text" className="form-control" id="last name" placeholder = {lastName}/>
-                </div>
-              </div>
-            <div className="form-group">
-              <label className="control-label col-sm-2 left" htmlFor="email">Email</label>
-              <div className="col-sm-10">
-                <input type="email" className="form-control" id="email" placeholder= {email} />
-                </div>
-              </div>
-                <div className="form-group">
-                  <label className="control-label col-sm-2 left" htmlFor="username" > Username</label>
-                  <div className="col-sm-10">
-                    <input type="text" className="form-control" id="username" placeholder = {username}/>
-                    </div>
+              <br></br>
+            <label className="control-label " htmlFor="first name" >First Name</label>
+              <input type="text" className="form-control" id="first name" placeholder = {firstName}/><br>
+              </br> <div className="form-group">
+                <label className="control-label " htmlFor="last name" >Last Name</label>
+                  <input type="text" className="form-control" id="last name" placeholder = {lastName}/>
                   </div>
-                      <div className="form-group">
-                      <label className="control-label col-sm-2 left" htmlFor="password" > Password</label>
-                      <div className="col-sm-10">
-                        <input type="text" className="form-control" id="password" placeholder = {password}/>
-                        </div>
+                </div>
+              <div className="form-group">
+                <label className="control-label " htmlFor="email">Email</label>
+                  <input type="email" className="form-control" id="email" placeholder= {email} />
+                  </div>
+
+                  <div className="form-group">
+                    <label className="control-label " htmlFor="username" > Username</label>
+                      <input type="text" className="form-control" id="username" placeholder = {username}/>
                       </div>
+
+                        <div className="form-group">
+                        <label className="control-label " htmlFor="password" > Password</label>
+                          <input type="text" className="form-control" id="password" placeholder = {password}/>
                         </div>
-                      </div>
+                        </div>
+                        </div>
+                        </div>
+
                     );
                   }
                 }
